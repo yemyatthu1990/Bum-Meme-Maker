3f950ce43827fdc039e82ccb235ed31f1aa8b51a
Ye Myat Thu <thu.yemyat@gmail.com>
2014-09-23 05:55:27 +0630

    Change from Public Constructor with Arguments to getNewInstance method.

--- app/src/main/java/com/yemyatthu/bummememaker/ConfirmDialogFragment.java
+++ app/src/main/java/com/yemyatthu/bummememaker/ConfirmDialogFragment.java
@@ -3,26 +3,30 @@ package com.yemyatthu.bummememaker;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 
 public class ConfirmDialogFragment extends DialogFragment{
 	public final String SEND_RESULT = "OK";
-	private int mMessage;
-	private int mTitle;
-	public ConfirmDialogFragment(int saveConfirm,int title)
+    public static final String TITLE_TAG = "com.yemyatthu.bummememaker.TITLE";
+    public static final String MESSAGE_TAG = "com.yemyatthu.bummememaker.MESSAGE";
+	public static ConfirmDialogFragment getNewInstance(int message,int title)
 	{	
-	
-		mMessage = saveConfirm;
-		mTitle = title;
+	    Bundle args = new Bundle();
+        args.putInt(TITLE_TAG, title);
+        args.putInt(MESSAGE_TAG,message);
+        ConfirmDialogFragment fragment = new ConfirmDialogFragment();
+        fragment.setArguments(args);
+        return fragment;
+
 	}
 	
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState){
-			return new AlertDialog.Builder(getActivity()).setMessage(mMessage).setTitle(mTitle).setIcon(R.drawable.ic_dialog_alert_holo_light)
+			return new AlertDialog.Builder(getActivity()).setMessage(getArguments().getInt(MESSAGE_TAG)).setTitle(getArguments().getInt(TITLE_TAG)).setIcon(R.drawable.ic_dialog_alert_holo_light)
 					.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener(){
 
 					@Override
