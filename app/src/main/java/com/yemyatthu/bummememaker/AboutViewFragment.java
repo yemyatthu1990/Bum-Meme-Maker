65d0dc94c55441236dafb53a34c754e3b503511c
Ye Myat Thu <thu.yemyat@gmail.com>
2014-09-24 04:01:45 +0630

    Change About.

--- app/src/main/java/com/yemyatthu/bummememaker/AboutViewFragment.java
+++ app/src/main/java/com/yemyatthu/bummememaker/AboutViewFragment.java
@@ -2,14 +2,18 @@ package com.yemyatthu.bummememaker;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutViewFragment extends Fragment {
 	@Override 
 	public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
-
-		return inflater.inflate(R.layout.fragment_about_view, container,false);
+        View v = inflater.inflate(R.layout.fragment_about_view, container,false);
+        TextView license = (TextView)v.findViewById(R.id.about_text_view);
+        license.setMovementMethod(new ScrollingMovementMethod());
+		return v;
 	}
 }
